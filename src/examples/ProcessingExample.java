37c3e760cfa1ab69692fba2f854c5c3c978ea075
Tim Steuer <tim.steuer@kom.tu-darmstadt.de>
2020-07-06 18:38:41 +0200

    Add missing dependencies (NER tagger)

--- src/examples/ProcessingExample.java
+++ src/examples/ProcessingExample.java
@@ -6,6 +6,6 @@ import intextbooks.ontologie.LanguageEnum;
 public class ProcessingExample {
 
 	public static void main(String args[]) {
-		TextbookProcessor.processFullTextbook("pah/to/textbook.pdf", LanguageEnum.ENGLISH, false, false, null, false, false, "email@email.com");
+		TextbookProcessor.processFullTextbook("textbooks/book.pdf", LanguageEnum.ENGLISH, false, false, null, false, false, "email@email.com");
 	}
 }
